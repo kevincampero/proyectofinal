9ed7259c9712ca8a4c7b7f554fa74942a4a9dd7d
kevincampero <kevincampero42@gmail.com>
2020-09-15 16:11:00 -0400

    ke

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-if(screen.width>600)
-{
-    //menu lateral boton
+//menu lateral boton
 const btn=document.querySelector('#menu-btn')
 const menu= document.querySelector('#sidemenu');
+if(screen.width>600)
+{
+    
 btn.addEventListener('click', e =>{
 menu.classList.toggle("menu-expanded");
 menu.classList.toggle("menu-collapsed");
