df99b87ddd842a5dff79369e1e0f6ba85a5e48e7
kevincampero <kevincampero42@gmail.com>
2020-09-15 14:49:45 -0400

    mensaje

--- js/app.js
+++ js/app.js
@@ -9,3 +9,8 @@ menu.classList.toggle("menu-collapsed");
 document.querySelector('body').classList.toggle("body-expanded")
 });
 }
+else
+{
+    alert("se abre solo en dispositivos con resolucion mayor a 600px")
+
+}
