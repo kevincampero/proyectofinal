dbab4bea0e0eac584d9d0feb48baf7259765acd0
kevincampero <kevincampero42@gmail.com>
2020-09-17 03:13:40 -0400

    asx

--- js/hospital.js
+++ js/hospital.js
@@ -21,12 +21,13 @@ else
 
 function mostrar(numapa)
 {
+    
     let element = document.getElementById(numapa);
     let elementStyle = window.getComputedStyle(element);
     let elementDisplay = elementStyle.getPropertyValue('display');
     if(elementDisplay=='none')
     {
-        document.getElementById(numapa).style.display="block"
+        document.getElementById(numapa).style.display="block";
         document.getElementsByClassName("containero").style.flexDirection="column";
     }
     else
