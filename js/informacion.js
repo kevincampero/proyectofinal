738f250f57b07406c94452af8b967dc3030b9ca6
kevincampero <kevincampero42@gmail.com>
2020-09-20 01:56:23 -0400

    cc

---
+++ js/informacion.js
@@ -0,0 +1,19 @@
+//menu lateral boton
+const btn=document.querySelector('#menu-btn')
+const menu= document.querySelector('#sidemenu');
+if(screen.width>600)
+{
+    
+btn.addEventListener('click', e =>{
+menu.classList.toggle("menu-expanded");
+menu.classList.toggle("menu-collapsed");
+document.querySelector('body').classList.toggle("body-expanded");
+
+});
+}
+else
+{
+    btn.addEventListener('click', e =>{
+    alert("se abre solo en dispositivos con resolucion mayor a 600px")
+    });
+}
